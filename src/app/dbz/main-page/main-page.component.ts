f936d5a3bdbae481c99031e0576ab5bf601c0c98
Alan Kralemann <akralemann@gmail.com>
2021-04-12 08:21:33 -0400

    Creacion de servicio para manejo de datos

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Personaje } from '../interfaces/dbz.interface';
-
+import { DbzService } from '../services/dbz.service';
 
 
 @Component({
@@ -9,33 +9,12 @@ import { Personaje } from '../interfaces/dbz.interface';
 })
 export class MainPageComponent {
 
-  personajes: Personaje [] = [
-    {
-    nombre : "Goku",
-    poder : 15000
-    },
-    {
-      nombre: "Vegeta",
-      poder: 8500
-    }
-];
 
   nuevo: Personaje = {
-    nombre: '',
-    poder: 0
+    nombre: 'Maestro Roshi',
+    poder: 1000
   }
 
-  agregar(){
-    if(this.nuevo.nombre.trim().length === 0){return;}
-    this.personajes.push(this.nuevo)
-    this.nuevo = {
-      nombre : '',
-      poder : 0
-    }
-  }
 
-  // cambiarNombre( event: any ){
-  //   console.log( event )
-  // }
-
+    constructor(){}
 }
